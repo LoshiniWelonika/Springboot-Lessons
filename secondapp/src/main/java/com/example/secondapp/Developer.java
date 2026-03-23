5cbbb0c07af50b2f04bde86e05dbffc2a6e2fc4b
LoshiniWelonika <welonikakedapanawala@gmail.com>
2026-03-23 23:14:07 +0530

    Dependency Injection First step

---
+++ secondapp/src/main/java/com/example/secondapp/Developer.java
@@ -0,0 +1,11 @@
+package com.example.secondapp;
+
+import org.springframework.stereotype.Component;
+
+@Component 
+public class Developer {
+    public void build(){
+        System.out.println("Working on Awesome Project");
+    }
+
+}
---
+++ secondapp/src/main/java/com/example/secondapp/SecondappApplication.java
@@ -0,0 +1,20 @@
+package com.example.secondapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SecondappApplication {
+
+	public static void main(String[] args) {
+
+		//Creating the object of IOC container and the type of IOC container is ApplicationContext
+		ApplicationContext context = SpringApplication.run(SecondappApplication.class, args);
+
+		Developer obj = context.getBean(Developer.class);
+
+		obj.build();
+	}
+
+}
